fd3c20dd03864f68cd7d7a474f23f074873d5683
humi-design <147498574+humi-design@users.noreply.github.com>
2026-02-17 14:26:59 +0530

    Modify DB_URL for SSL connection
    Updated database URL to include sslmode requirement.

--- streamlit_job_card_app.py
+++ streamlit_job_card_app.py
@@ -14,7 +14,7 @@ st.set_page_config(page_title="Factory Job Card System",
                    page_icon="🏭",
                    layout="wide")
 
-DB_URL = "postgresql://postgres:Ralpana0987@db.ibfqpjpvyxnvditlaayg.supabase.co:5432/postgres"
+DB_URL = "postgresql://postgres:Ralpana09876@db.ibfqpjpvyxnvditlaayg.supabase.co:5432/postgres?sslmode=require"
 
 # -----------------------------
 # DB CONNECTION
